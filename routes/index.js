39dc9fb020e90483b622335e9785f33f2513f876
raihan2604 <raihan2604@gmail.com>
2021-03-06 21:30:03 +0700

    CRD finished

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var fetch = require('node-fetch');
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let url = "http://localhost:8000/users";
@@ -25,6 +26,42 @@ router.get('/user/:id',(req,res,next) => {
     .then(json => {
       res.render('getUser', { user:json[0] });
     });
+});
+
+router.get('/create-user', (req,res,next) => {
+  res.render("createUser");
+});
+
+router.post('/create-user', (req,res,next) => {
+  const requestBody = {
+    name: req.body.name,
+    address: req.body.address,
+    dob: req.body.dob,
+    gender:req.body.gender
+  };
+
+  let url = "http://localhost:8000/users/add";
+
+  fetch(url,{ 
+    method: "POST",
+    body: JSON.stringify(requestBody),
+    headers: { 'Content-Type': 'application/json' }
+  })
+   .then(result => {
+     res.redirect('/');
+   }).catch(error => console.log(error));
+});
+
+router.get('/delete-user/:id',(req,res,next) => {
+  let url = "http://localhost:8000/users/del/"+req.params.id;
+  let option = { method: "DELETE" };
+
+  fetch(url,option)
+    .then(result => {
+      res.redirect('/');
+    })
+    .catch(error => console.log(error));
+
 });
 
 module.exports = router;
