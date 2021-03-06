142ecd25d2fc07d9a667de0aebd2a9eac37f1e87
raihan2604 <raihan2604@gmail.com>
2021-03-06 20:20:11 +0700

    finish user and user detail

--- app.js
+++ app.js
@@ -38,6 +38,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(8000);
-
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var fetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Test' });
+  let url = "http://localhost:8000/users";
+  let option = { method: "GET" };
+  let users = [];
+  fetch(url,option)
+    .then(res => res.json())
+    .catch(error => console.log(error))
+    .then(json => {
+      users = json;
+      res.render('index', { users: users });
+    });
+});
+
+router.get('/user/:id',(req,res,next) => {
+  let url = "http://localhost:8000/users/get/"+req.params.id;
+  let option = { method: "GET" };
+  fetch(url,option)
+    .then(res => res.json())
+    .catch(error => console.log(error))
+    .then(json => {
+      res.render('getUser', { user:json[0] });
+    });
 });
 
 module.exports = router;
