81cd7171abf7319363c4f327dc5939638afcedfa
raihan2604 <raihan2604@gmail.com>
2021-03-09 15:21:44 +0700

    finish

--- routes/index.js
+++ routes/index.js
@@ -64,4 +64,68 @@ router.get('/delete-user/:id',(req,res,next) => {
 
 });
 
+router.get('/update-user/:id', (req,res,next) => {
+  let url = "http://localhost:8000/users/get/"+req.params.id;
+  let option = { method: "GET" };
+  fetch(url,option)
+    .then(res => res.json())
+    .catch(error => console.log(error))
+    .then(json => {
+      res.render('updateUser', { user:json[0] });
+    });
+});
+
+router.post('/update-user/:id',(req,res,next) =>{
+  const requestBody = {
+    name: req.body.name,
+    address: req.body.address,
+    dob: req.body.dob,
+    gender:req.body.gender
+  };
+
+  let url = "http://localhost:8000/users/update-user/"+req.params.id;
+
+  fetch(url,{ 
+    method: "PUT",
+    body: JSON.stringify(requestBody),
+    headers: { 'Content-Type': 'application/json' }
+  })
+   .then(result => {
+     res.redirect('/user/'+req.params.id);
+   }).catch(error => console.log(error));
+});
+
+router.get('/add-books/:id', (req,res,next) =>{
+  let url = "http://localhost:8000/users/get/"+req.params.id;
+  let option = { method: "GET" };
+  fetch(url,option)
+    .then(res => res.json())
+    .catch(error => console.log(error))
+    .then(json => {
+      res.render('addBooks', { user:json[0] });
+    });
+});
+
+router.post('/add-books/:id',(req,res,next) => {
+  const requestBody = {
+    books: [
+      {
+        bookId: req.body.bookId,
+        bookName: req.body.bookName
+      }
+    ]
+  };
+
+  let url = "http://localhost:8000/users/update-user/"+req.params.id;
+
+  fetch(url,{ 
+    method: "PUT",
+    body: JSON.stringify(requestBody),
+    headers: { 'Content-Type': 'application/json' }
+  })
+   .then(result => {
+     res.redirect('/user/'+req.params.id);
+   }).catch(error => console.log(error));
+});
+
 module.exports = router;
